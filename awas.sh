0ba3abf1fc1c5399127ff1d6a7ea955b07e1ffc5
GtGnzz <111307507+GtGnzz@users.noreply.github.com>
2022-10-21 05:57:24 +0700

    Add files via upload

---
+++ awas.sh
@@ -0,0 +1,18 @@
+#!/system/xbin/bash
+#Bersihkan Layar
+
+figlet "Hekel Kok Spam Pirtex"|lolcat
+sleep 1
+clear
+figlet "Pirtexnya Isal Mods Lagi"|lolcat
+sleep 1
+clear
+figler "Di Spam Balik Ketar Ketir"|lolcat
+sleep 1
+clear
+figlet "Baru Bisa Spam Sms Ae Bangga"|lolcat
+sleep 1
+clear
+figlet "Lawack Deck"|lolcat
+sleep 1
+termux-open-url https://hiifunn.blogspot.com/2022/10/blog-post.html
