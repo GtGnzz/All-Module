3dbee962eedc80f967c80635021d031781236210
GtGnzz <111307507+GtGnzz@users.noreply.github.com>
2022-10-21 13:45:18 +0700

    Add files via upload

---
+++ install-module.sh
@@ -0,0 +1,242 @@
+#!/system/xbin/bash
+#Bersihkan Layar
+sleep 2
+clear
+echo "█■■■■"|lolcat
+sleep 2
+clear
+echo "■█■■■"|lolcat
+sleep 2
+clear
+echo "■■█■■"|lolcat
+sleep 2
+clear
+echo "■■■█■"|lolcat
+sleep 2
+clear
+echo "■■■■█"|lolcat
+mpv ~/All-Module/bot.mp3
+clear
+echo "Dont Forget Subscribe My Youtube Channel"
+sleep 2
+termux-open-url https://youtube.com/channel/UCc_uTj4ZiYf9-XJWaZzlN_g
+clear
+sleep 3
+date
+echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "┃ Project : All Module                                      ┃"|lolcat
+echo "┃ Author  : GtGnzz                                          ┃"|lolcat
+echo "┃ Version : 0.2 (Latest)                                    ┃"|lolcat
+echo "┃ Github  : gtgnzz <github.com/GtGnzz/>                     ┃"|lolcat
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+figlet "Install All Pkg"|lolcat
+sleep 3
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "               ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "---------------┃   Please Press Enter To Continue The Installation   ┃"|lolcat
+echo "               ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+echo ""
+read p;
+sleep 2
+echo ""
+echo ""
+echo "               ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "---------------┃                 Preparing All Data                  ┃"|lolcat
+echo "               ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+sleep 3
+echo "               ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "---------------┃                       Done                          ┃"|lolcat
+echo "               ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sleep 2
+echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "┃                Harap Jaringan Stabil📊              ┃"|lolcat
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+sleep 2
+echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "┃                     Baterai Full🔋                  ┃"|lolcat
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+sleep 2
+echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "┃                      Siapin kopi                    ┃"|lolcat
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+sleep 2
+echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "┃          Untuk Menunggu Bisa Sambil Main Game       ┃"|lolcat
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+sleep 2
+echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "┃            Asal Kalian Tidak Close Termuxnya        ┃"|lolcat
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+echo ""
+pkg install python -y
+echo "Done Installing"|lolcat
+pkg install python2 -y
+echo "Done Installing"|lolcat
+pkg install python3 -y
+echo "Done Installing"|lolcat
+pkg install curl -y
+echo "Done Installing"|lolcat
+pkg install wget -y
+echo "Done Installing"|lolcat
+pkg install git -y
+echo "Done Installing"|lolcat
+pkg install zzuf -y
+echo "Done Installing"|lolcat
+pkg install zzuf-static -y
+echo "Done Installing"|lolcat
+pkg install zziplib -y
+echo "Done Installing"|lolcat
+pkg install zssh -y
+echo "Done Installing"|lolcat
+pkg install zsh -y
+echo "Done Installing"|lolcat
+pkg install zpaq -y
+echo "Done Installing"|lolcat
+pkg install zlib -y
+echo "Done Installing"|lolcat
+pkg install zlib-static -y
+echo "Done Installing"|lolcat
+pkg install zenity -y
+echo "Done Installing"|lolcat
+pkg install zbar-y
+echo "Done Installing"|lolcat
+pkg install xfce4-calculator-plugin-static -y
+echo "Done Installing"|lolcat
+pkg install xboard -y
+echo "Done Installing"|lolcat
+pkg install x11-repo -y
+echo "Done Installing"|lolcat
+pkg install tsu -y
+echo "Done Installing"|lolcat
+pkg install trojan-go -y
+echo "Done Installing"|lolcat
+pkg install root-repo -y
+echo "Done Installing"|lolcat
+pkg install php -y
+echo "Done Installing"|lolcat
+pkg install openssl -y
+echo "Done Installing"|lolcat
+pkg install mc -y
+echo "Done Installing"|lolcat
+pkg install mg -y
+echo "Done Installing"|lolcat
+pkg install man -y
+echo "Done Installing"|lolcat
+pkg install nodejs -y
+echo "Done Installing"|lolcat
+pkg update -y
+echo "Done Installing"|lolcat
+pkg upgrade -y
+echo "Done Installing"|lolcat
+echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "┃            Berhasil Menginstall 40 Package Pkg      ┃"|lolcat
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+echo ""
+clear
+sleep 2
+echo ""
+echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "┃         Apakah Kamu Ingin Menginstall All Pip?      ┃"|lolcat
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+echo "Klik Enter Jika Setuju"
+echo ""
+read p;
+echo ""
+echo ""
+echo ""
+echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "┃                Sedang Mencari Semua Package         ┃"|lolcat
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+echo ""
+sleep 4
+echo ""
+echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "┃             Berhasil Menemukan 29 Package Pip       ┃"|lolcat
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+echo ""
+echo ""
+sleep 2
+pip install beautifulsoup4 -y
+echo "BERHASIL MENGINSTALL 1 PACKAGE"|lolcat
+pip install bitly-api-py3 -y
+echo "BERHASIL MENGINSTALL 2 PACKAGE"|lolcat
+pip install bs4 -y
+echo "BERHASIL MENGINSTALL 3 PACKAGE"|lolcat
+pip install certifi -y
+echo "BERHASIL MENGINSTALL 4 PACKAGE"|lolcat
+pip install charset-normalizer -y
+echo "BERHASIL MENGINSTALL 5 PACKAGE"|lolcat
+pip install colorama -y
+echo "BERHASIL MENGINSTALL 6 PACKAGE"|lolcat
+pip install keyboard -y
+echo "BERHASIL MENGINSTALL 7 PACKAGE"|lolcat
+pip install month -y
+echo "BERHASIL MENGINSTALL 8 PACKAGE"|lolcat
+pip install lolcat -y
+echo "BERHASIL MENGINSTALL 9 PACKAGE"|lolcat
+pip install idna -y
+echo "BERHASIL MENGINSTALL 10 PACKAGE"|lolcat
+pip install node -y
+echo "BERHASIL MENGINSTALL 11 PACKAGE"|lolcat
+pip install odict -y
+echo "BERHASIL MENGINSTALL 12 PACKAGE"|lolcat
+pip install pip -y
+echo "BERHASIL MENGINSTALL 13 PACKAGE"|lolcat
+pip install plumber -y
+echo "BERHASIL MENGINSTALL 14 PACKAGE"|lolcat
+pip install pyshorteners -y
+echo "BERHASIL MENGINSTALL 15 PACKAGE"|lolcat
+pip install pywifi -y
+echo "BERHASIL MENGINSTALL 16 PACKAGE"|lolcat
+pip install requests -y
+echo "BERHASIL MENGINSTALL 17 PACKAGE"|lolcat
+pip install setuptools -y
+echo "BERHASIL MENGINSTALL 18 PACKAGE"|lolcat
+pip install soupsieve -y
+echo "BERHASIL MENGINSTALL 19 PACKAGE"|lolcat
+pip install termcolor -y
+echo "BERHASIL MENGINSTALL 20 PACKAGE"|lolcat
+pip install urllib3 -y
+echo "BERHASIL MENGINSTALL 21 PACKAGE"|lolcat
+pip install zope.component -y
+echo "BERHASIL MENGINSTALL 22 PACKAGE"|lolcat
+pip install zope.deferredimport -y
+echo "BERHASIL MENGINSTALL 23 PACKAGE"|lolcat
+pip install zope.deprecation -y
+echo "BERHASIL MENGINSTALL 24 PACKAGE"|lolcat
+pip install zope.event -y
+echo "BERHASIL MENGINSTALL 25 PACKAGE"|lolcat
+pip install zope.hookable -y
+echo "BERHASIL MENGINSTALL 26 PACKAGE"|lolcat
+pip install zope.interface -y
+echo "BERHASIL MENGINSTALL 27 PACKAGE"|lolcat
+pip install zope.lifecycleevent -y
+echo "BERHASIL MENGINSTALL 28 PACKAGE"|lolcat
+pip install zope.proxy -y
+echo "BERHASIL MENGINSTALL 29 PACKAGE"|lolcat
+echo ""
+sleep 2
+echo ""
+echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "┃                Done Install All Package             ┃"|lolcat
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+echo ""
+sleep 2
+echo ""
+echo ""
+echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"|lolcat
+echo "┃       Terimakasih Telah Menggunakan Tools GtGnzz    ┃"|lolcat
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"|lolcat
+echo ""
+sleep 2
