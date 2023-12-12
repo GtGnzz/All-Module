5a43f9203e41b25ed0e339c092fe9e44d8497906
GtGnzz <111307507+GtGnzz@users.noreply.github.com>
2023-12-12 23:14:19 +0700

    install-module.sh

---
+++ gas/install-module.sh
@@ -0,0 +1,286 @@
+#!/system/xbin/bash
+#Bersihkan Layar
+sleep 0.5
+clear
+figlet -f ASCII-Shadow "\    Module"| lolcat
+echo ""
+echo ""
+printf "\rWAITING. . ."
+sleep 0.5
+printf "\rOKEH GAS..."
+sleep 0.5
+printf "\rNYALAIN MUSIC DULU"
+mpv ~/All-Module/bot.mp3
+clear
+echo "JANGAN LUPA SUBSCRIBE CHANNEL GW DULU YA BRO"
+sleep 2
+termux-open-url https://youtube.com/@gtgnzz
+clear
+sleep 3
+date
+echo "      █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"|lolcat
+echo "      █ Project : All Module                                      █"|lolcat
+echo "      █ Author  : GtGnzz                                          █"|lolcat
+echo "      █ Version : 1.0 (Latest)                                    █"|lolcat
+echo "      █ Github  : gtgnzz <github.com/GtGnzz/>                     █"|lolcat
+echo "      █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"|lolcat
+echo "                          𝕀𝕟𝕤𝕥𝕒𝕝𝕝 𝕄𝕠𝕕𝕦𝕝𝕖"|lolcat
+sleep 3
+echo "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"|lolcat
+echo "            Please Press Enter To Continue The Installation                 "|lolcat
+echo "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"|lolcat
+echo ""
+echo ""
+echo ""
+echo ""
+read p;
+sleep 2
+echo "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"|lolcat
+echo "                          Preparing All Data                                "|lolcat
+echo "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"|lolcat
+sleep 3
+echo ""
+echo ""
+echo ""
+echo ""
+echo "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"|lolcat
+echo "                                 Done                                       "|lolcat
+echo "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"|lolcat
+sleep 2
+echo ""
+echo ""
+echo ""
+echo ""
+echo "    █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"|lolcat
+echo "      ﮩ٨ـﮩﮩ٨ـﮩﮩJika Ada Pilihan Y/N Kalian Tinggal Ketikan Yﮩﮩـ٨ﮩﮩـ٨ﮩ   "|lolcat
+echo "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"|lolcat
+sleep 2
+
+
+termux-setup-storage
+clear
+
+pkg install python
+echo "Done Installing"|lolcat
+clear
+
+pkg install python2
+echo "Done Installing"|lolcat
+clear
+
+pkg install python3
+echo "Done Installing"|lolcat
+clear
+
+pkg install curl
+echo "Done Installing"|lolcat
+clear
+
+pkg install wget
+echo "Done Installing"|lolcat
+clear
+
+pkg install git
+echo "Done Installing"|lolcat
+clear
+
+pkg install zzuf
+echo "Done Installing"|lolcat
+clear
+
+pkg install zzuf-static
+echo "Done Installing"|lolcat
+clear
+
+pkg install zziplib
+echo "Done Installing"|lolcat
+clear
+
+pkg install zssh
+echo "Done Installing"|lolcat
+clear
+
+pkg install zsh
+echo "Done Installing"|lolcat
+clear
+
+pkg install zpaq
+echo "Done Installing"|lolcat
+clear
+
+pkg install zlib
+echo "Done Installing"|lolcat
+clear
+
+pkg install zlib-static
+echo "Done Installing"|lolcat
+clear
+
+pkg install zenity
+echo "Done Installing"|lolcat
+clear
+
+pkg install zbar
+echo "Done Installing"|lolcat
+clear
+
+pkg install xfce4-calculator-plugin-static
+echo "Done Installing"|lolcat
+clear
+
+pkg install xboard
+echo "Done Installing"|lolcat
+clear
+
+pkg install x11-repo
+echo "Done Installing"|lolcat
+clear
+
+pkg install tsu
+echo "Done Installing"|lolcat
+clear
+
+pkg install trojan-go
+echo "Done Installing"|lolcat
+clear
+
+pkg install root-repo
+echo "Done Installing"|lolcat
+clear
+
+pkg install php
+echo "Done Installing"|lolcat
+clear
+
+pkg install openssl
+echo "Done Installing"|lolcat
+clear
+
+pkg install mc
+echo "Done Installing"|lolcat
+clear
+
+pkg install mg
+echo "Done Installing"|lolcat
+clear
+
+pkg install man
+echo "Done Installing"|lolcat
+clear
+
+pkg install nodejs
+echo "Done Installing"|lolcat
+clear
+
+pkg update
+echo "Done Installing"|lolcat
+clear
+
+pkg upgrade
+
+clear
+echo "Done Installing"|lolcat
+
+echo "    █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"|lolcat
+echo "                         Done Installing 40 Package                     "|lolcat
+echo "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"|lolcat
+clear
+sleep 2
+echo ""
+echo "    █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"|lolcat
+echo "                      Apakah Kamu Ingin Menginstall Pip?                "|lolcat
+echo "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"|lolcat
+echo "                          Klik Enter Jika Setuju"
+echo ""
+read p;
+echo ""
+echo ""
+echo ""
+echo "    █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"|lolcat
+echo "                       Sedang Mencari Semua Module Pip                  "|lolcat
+echo "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"|lolcat
+sleep 1
+echo ""
+echo "    █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"|lolcat
+echo "                         Module Pip Siap Diinstall                      "|lolcat
+echo "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"|lolcat
+sleep 2
+pip install beautifulsoup4
+figlet "➤➤➤➤➤"|lolcat
+pip install bitly-api-py3
+figlet "➤➤➤➤➤"|lolcat
+pip install bs4
+figlet "➤➤➤➤➤"|lolcat
+pip install certifi
+figlet "➤➤➤➤➤"|lolcat
+pip install charset-normalizer
+figlet "➤➤➤➤➤"|lolcat
+pip install colorama
+figlet "➤➤➤➤➤"|lolcat
+pip install keyboard
+figlet "➤➤➤➤➤"|lolcat
+pip install month
+figlet "➤➤➤➤➤"|lolcat
+pip install lolcat
+figlet "➤➤➤➤➤"|lolcat
+pip install idna
+figlet "➤➤➤➤➤"|lolcat
+pip install node
+figlet "➤➤➤➤➤"|lolcat
+pip install odict
+figlet "➤➤➤➤➤"|lolcat
+pip install pip
+figlet "➤➤➤➤➤"|lolcat
+pip install plumber
+figlet "➤➤➤➤➤"|lolcat
+pip install pyshorteners
+figlet "➤➤➤➤➤"|lolcat
+pip install pywifi
+figlet "➤➤➤➤➤"|lolcat
+pip install requests
+figlet "➤➤➤➤➤"|lolcat
+pip install setuptools
+figlet "➤➤➤➤➤"|lolcat
+pip install soupsieve
+figlet "➤➤➤➤➤"|lolcat
+pip install termcolor
+figlet "➤➤➤➤➤"|lolcat
+pip install urllib3
+figlet "➤➤➤➤➤"|lolcat
+pip install zope.component
+figlet "➤➤➤➤➤"|lolcat
+pip install zope.deferredimport
+figlet "➤➤➤➤➤"|lolcat
+pip install zope.deprecation
+figlet "➤➤➤➤➤"|lolcat
+pip install zope.event
+figlet "➤➤➤➤➤"|lolcat
+pip install zope.hookable
+figlet "➤➤➤➤➤"|lolcat
+pip install zope.interface
+figlet "➤➤➤➤➤"|lolcat
+pip install zope.lifecycleevent
+figlet "➤➤➤➤➤"|lolcat
+pip install zope.proxy
+figlet "➤➤➤➤➤"|lolcat
+echo ""
+clear
+sleep 2
+echo ""
+echo "    █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"|lolcat
+echo "                        Semuanya Sudah Berhasil Diinstall               "|lolcat
+echo "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"|lolcat
+sleep 2
+echo ""
+echo ""
+echo "    █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"|lolcat
+echo "                     Terimakasih Telah Menggunakan Tools Ini            "|lolcat
+echo "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"|lolcat
+sleep 2
+echo ""
+echo ""
+echo "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"|lolcat
+echo "   Tujuan: Supaya Tidak Terjadi Error Saat Kalian Lupa/Tidak Menginstall  "|lolcat
+echo "               Package Yang Di Butuhkan Script Tersebut                   "|lolcat
+echo "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"|lolcat
+
